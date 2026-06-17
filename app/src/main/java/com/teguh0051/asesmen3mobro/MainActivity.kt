a2602ca0dc64e580ca29a2662736e9d635d79001
MUHAMMAD TEGUH BUDIMAN <mteguhb118@gmail.com>
2026-06-17 21:24:23 +0700

    Initial commit

---
+++ app/src/main/java/com/teguh0051/asesmen3mobro/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.teguh0051.asesmen3mobro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.teguh0051.asesmen3mobro.ui.theme.Asesmen3mobroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Asesmen3mobroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Asesmen3mobroTheme {
+        Greeting("Android")
+    }
+}
